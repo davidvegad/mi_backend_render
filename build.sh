c1a58c3f27008ed8d50f1eb9c6ac28d7fa577e84
davidvegacorus <david.vega@corusconsulting.com>
2025-06-25 16:25:11 -0500

    Add build script for Render and non-interactive superuser

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# 1. Instalar dependencias
+pip install -r requirements.txt
+
+# 2. Correr las migraciones para crear las tablas de la BD
+python manage.py migrate
+
+# 3. Recolectar los archivos estáticos
+python manage.py collectstatic --no-input
+
+# 4. Crear el superusuario de forma no interactiva
+#    (leerá las variables de entorno DJANGO_SUPERUSER...)
+python manage.py createsuperuser --noinput
